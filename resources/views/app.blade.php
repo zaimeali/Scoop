56913a3bf8c09757c61830da0071acda93356342
abdul-raffay <abdul.raffay@nytrotech.com>
2020-07-11 15:04:50 +0500

    React Configured

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        {{-- CSRF TOKEN --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Scoop</title>
+
+        {{-- CSS --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body>
+        <div id="app"></div>
+
+        {{-- JS --}}
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
